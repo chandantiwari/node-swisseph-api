c7f5701a1c6fb7f30dad9b2476d96ff757c46821
chandantiwari <chandanrishiraj@gmail.com>
2015-05-06 15:39:48 +0530

    Initial skeleton for express app

---
+++ app.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+var routes = require('./routes/index');
+
+// instantiating express app
+var app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/', routes);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.json({
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
