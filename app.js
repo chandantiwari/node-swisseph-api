3385cadaee37278750f1ebebf5e4e3cd52207d74
Chandan Tiwari <crt>
2015-05-18 19:16:30 +0530

    Added some functionalities to the Astronomy module and little refactoring

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
-var routes = require('./routes/index');
+var api = require('./routes/api');
 
 // instantiating express app
 var app = express();
@@ -10,7 +10,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/', routes);
+app.use('/', api);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- core/astronomy.js
+++ core/astronomy.js
@@ -1,17 +1,139 @@
 /**
  * Created by chandan on 09/05/15.
  */
+var swisseph = require('node-swisseph');
+var async = require('async');
+
+var flag = swisseph.FLG_SPEED;
+swisseph.set_ephe_path(__dirname + '/../ephe');
+
+function getCorrectedUTCTime(data)
+{
+    var utcDate,
+        whole,
+        fraction,
+        correctedMin,
+        correctedHour;
+
+    if(data.timezone >= 0)
+    {
+        whole = Math.floor(data.timezone);
+        fraction = data.timezone - whole;
+    }
+    else
+    {
+        whole  = Math.ceil(data.timezone);
+        fraction = data.timezone - whole;
+    }
+    correctedHour = data.hour - whole;
+    correctedMin = data.minute - (fraction * 60);
+    utcDate = new Date(data.year, data.month - 1, data.date, correctedHour, correctedMin);
+
+    return utcDate;
+}
+
+function getJulianDay(data, callback)
+{
+    var utcDate = getCorrectedUTCTime(data);
+    swisseph.julday(utcDate.getFullYear(), utcDate.getMonth()+1, utcDate.getDate(), utcDate.getHours() + (utcDate.getMinutes()/60), swisseph.GREG_CAL, function (julday_ut) {
+        callback(null, julday_ut);
+    });
+}
 
 
+// calculate everything starting from planet positions, speeds, jd, ayanamsha, ascendant.
+function calcAstronomicalData(data, callback)
+{
+    getJulianDay(data, function(error, jd){
+
+        if(error)
+        {
+            return callback(error);
+        }
+
+        async.parallel({
+
+            sun : function(callback){
+                swisseph.calc_ut(jd, swisseph.SUN, flag, function (body)
+                {
+                    callback(null, body);
+                });
+            },
+            moon : function(callback){
+                swisseph.calc_ut(jd, swisseph.MOON, flag, function (body)
+                {
+                    callback(null, body);
+                });
+            },
+            mars : function(callback){
+                swisseph.calc_ut(jd, swisseph.MARS, flag, function (body)
+                {
+                    callback(null, body);
+                });
+            },
+            mercury : function(callback){
+                swisseph.calc_ut(jd, swisseph.MERCURY, flag, function (body)
+                {
+                    callback(null, body);
+                });
+            },
+            jupiter : function(callback){
+                swisseph.calc_ut(jd, swisseph.JUPITER, flag, function (body)
+                {
+                    callback(null, body);
+                });
+            },
+            venus : function(callback){
+                swisseph.calc_ut(jd, swisseph.VENUS, flag, function (body)
+                {
+                    callback(null,body);
+                });
+            },
+            saturn : function(callback){
+                swisseph.calc_ut(jd, swisseph.SATURN, flag, function (body)
+                {
+                    callback(null, body);
+                });
+            },
+            uranus : function(callback){
+                swisseph.calc_ut(jd, swisseph.URANUS, flag, function (body)
+                {
+                    callback(null, body);
+                });
+            },
+            neptune : function(callback){
+                swisseph.calc_ut(jd, swisseph.NEPTUNE, flag, function (body)
+                {
+                    callback(null, body);
+                });
+            },
+            pluto : function(callback){
+                swisseph.calc_ut(jd, swisseph.PLUTO, flag, function (body)
+                {
+                    callback(null, body);
+                });
+            }
+
+        }, function(error, results){
+
+            if(!error)
+            {
+                return callback(null, results);
+            }
+        });
+
+    });
+
+}
 
 var api = {
 
-    getJulianDay : function(data){
-
+    getJulianDay : function(data, callback){
+        getJulianDay(data, callback);
     },
 
     getSolarSystemBodies : function(data, callback){
-
+        calcAstronomicalData(data, callback);
     },
 
     getLunarPosition : function(data, callback){
@@ -19,6 +141,10 @@ var api = {
     },
 
     getSolarPosition : function(data, callback){
+
+    },
+
+    getBodyPositionById : function(data, callback){
 
     }
 };
